b91589a4b8ecb96bf3f5479b15c0023b40de7ae4
WhizSid <whizsid@aol.com>
2024-10-17 12:13:28 +0530

    Added example to timeout

--- examples/nodejs-example/index.js
+++ examples/nodejs-example/index.js
@@ -9,6 +9,10 @@ import * as example from "./pkg/nodejs_example.js";
   console.log("Interval JS");
   await example.interval_test();
   console.log("Interval End JS");
+
+  console.log("Timeout Start JS");
+  await example.timeout_test();
+  console.log("Timeout Finished JS");
   process.exit(0);
 })().catch((e) => {
   console.error(e);
--- examples/nodejs-example/src/lib.rs
+++ examples/nodejs-example/src/lib.rs
@@ -3,8 +3,7 @@ mod utils;
 use std::time::Duration;
 
 use wasm_bindgen::prelude::*;
-use wasmtimer::tokio::sleep;
-use wasmtimer::tokio::interval;
+use wasmtimer::tokio::{interval, sleep, timeout};
 use web_sys::console::log_1;
 
 #[wasm_bindgen]
@@ -27,4 +26,26 @@ pub async fn interval_test() {
     interval.tick().await;
     log_1(&JsValue::from_str("Interval Rust 5"));
     drop(interval);
+}
+
+#[wasm_bindgen]
+pub async fn timeout_test() {
+    log_1(&JsValue::from_str("Timeout Start Rust"));
+    let result = timeout(Duration::from_secs(1), async {
+        log_1(&JsValue::from_str("Timeout Callback Rust"));
+        sleep(Duration::from_secs(3)).await;
+        log_1(&JsValue::from_str("Timeout Failed 1 Rust"));
+    })
+    .await;
+    match result {
+        Ok(_) => {
+            log_1(&JsValue::from_str("Timeout Failed 2 Rust"));
+        }
+        Err(e) => {
+            log_1(&JsValue::from_str(&format!(
+                "Timeout Success. Error:- {:?}",
+                e
+            )));
+        }
+    }
 }
